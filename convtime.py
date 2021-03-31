fe9d7e4f037ff3a41c6b16ae3d7c5885aeb1a682
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-31 12:37:39 +0200

    convert is done

--- convtime.py
+++ convtime.py
@@ -1,15 +1,19 @@
-import time
 
 print("[1] Zamiana jednostek..")
 print("[2] Dokladny czas lokalny..")
+
 wybor = int(input("Pozycja z menu =====> "))
 
+def binsys():
+  print("Zamiana z liczby binarnej na inne....")
+  x = int(input("Podaj liczbe binarna ===> "))
+
+  print("hex => ",hex(x))
+  print("oct => ",oct(x))
+  print("bin => ",bin(x))
+
 if wybor == 1:
- bin()
+  binsys()
 elif wybor == 2:
- print("")
+  print("ok")
 
-
-def bin():
- print("Zamiana z liczby binarnej na inne....")
- bin = int(input("Podaj liczbe binarna ===> "))
