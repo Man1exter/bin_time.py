a2f40e9330ba8fc36c696989ba486b57ca4b9594
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-31 14:27:09 +0200

    all convereter functions/ele is done

--- convtime.py
+++ convtime.py
@@ -1,19 +1,38 @@
 
 print("[1] Zamiana jednostek..")
 print("[2] Dokladny czas lokalny..")
+print(" ")
+print(" ")
 
 wybor = int(input("Pozycja z menu =====> "))
 
 def binsys():
-  print("Zamiana z liczby binarnej na inne....")
-  x = int(input("Podaj liczbe binarna ===> "))
+  print("[1] Zamiana z liczby binarnej na inne....")
+  print("[2] Z innych na binarna...")
+  print(" ")
+  print(" ")
 
-  print("hex => ",hex(x))
-  print("oct => ",oct(x))
-  print("bin => ",bin(x))
+  wybor2 = int(input("Pozycja z menu =====> "))
+
+  if wybor2 == 1:
+   x = int(input("Podaj liczbe binarna ===> "))
+
+   print("hex => ",hex(x))
+   print("oct => ",oct(x))
+   print("bin => ",bin(x))
+
+  elif wybor2 == 2:
+   y = input("Podaj liczbe binarna ===> ")
+
+   print("na dwojkowy ==>",int(y,2))
+   print("na osemkowy ==>",int(y,8))
+   print("na szesnastkowy ==>",int(y,16))
+   
 
 if wybor == 1:
   binsys()
 elif wybor == 2:
   print("ok")
+else:
+  print("nie wiem co chcesz zrobic..")
 
