b7850e6ba18669d4fa1d8af958f2274bf45eb6be
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-31 12:21:56 +0200

    menu + choice

--- convtime.py
+++ convtime.py
@@ -1,1 +1,15 @@
 import time
+
+print("[1] Zamiana jednostek..")
+print("[2] Dokladny czas lokalny..")
+wybor = int(input("Pozycja z menu =====> "))
+
+if wybor == 1:
+ bin()
+elif wybor == 2:
+ print("")
+
+
+def bin():
+ print("Zamiana z liczby binarnej na inne....")
+ bin = int(input("Podaj liczbe binarna ===> "))
